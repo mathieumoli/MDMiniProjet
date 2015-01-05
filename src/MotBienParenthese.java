68dec7e3513f66b82522db0cbf822a23ca8c02c2
user <user@Axxx>
2015-01-05 21:13:59 +0100

    partie1 done

--- src/MotBienParenthese.java
+++ src/MotBienParenthese.java
@@ -1,8 +1,23 @@
 import java.util.ArrayList;
 
+/**
+ * Première partie : Mots bien parenthésés et nombres de Catalan
+ * 
+ * @author PARIS Simon
+ * @version 2015.03.01
+ */
 
 public class MotBienParenthese {
     
+    /**
+     * Affiche tous les mots bien paranthésés de taille 2*n.
+     * le schema inductif utilisé est 'm = (m1)m2' avec m1 et m2 mot BP. La base est le mot vide.
+     * 
+     * Complexité THEORIQUE exponentielle, O(e(n))
+     * 
+     * @param n
+     *          mot de taille 2*n
+     */   
     public static ArrayList<String> enumMotsBP(int n) {// (m1)m2
         ArrayList<String> res = new ArrayList<String>();
         if (n == 0) {
@@ -19,6 +34,17 @@ public class MotBienParenthese {
         return res;
     }
     
+    /**
+     * Multiplie 2 ArrayList est ajoute (x) aux éléments de la première ArrayList.
+     * ex: multiList({a,b},{c,d} -> {(a)c, (a)d, (b)c, (b)d}
+     * 
+     * Complexité Quadratique, O(n²)
+     * 
+     * @param m1
+     *          ArrayList de mot BP
+     * @param m2
+     *          ArrayList de mot BP
+     */   
     public static ArrayList<String> multiList(ArrayList<String> m1, ArrayList<String> m2) {
         ArrayList<String> res = new ArrayList<String>();
         for (String i : m1)  {
@@ -29,7 +55,16 @@ public class MotBienParenthese {
         return res;
     }
     
-//C(0) = 1 et C(n+1) = SUM[0:n](C(i)*C(n-i))   
+    /**
+     * Calcul du nombre de Catalan à l'aide de la formule suivant:
+     * C(0) = 1 et C(n+1) = SUM[0:n](C(i)*C(n-i)))
+     * le nombre de catalan(n) correspond au nombre de mot BP de taille 2*n
+     * 
+     * Complexité exponentielle, O(e(n))
+     * 
+     * @param n
+     *          mot de taille 2*n
+     */   
     public static int catalan(int n) {
         if (n == 0) {
             return 1;
@@ -42,6 +77,15 @@ public class MotBienParenthese {
         }
     }
     
+    /**
+     * Calcul du nombre de Catalan de manière itératif.
+     * le nombre de catalan(n) correspond au nombre de mot BP de taille 2*n
+     * 
+     * Complexité quadratique, O(n²)
+     * 
+     * @param n
+     *          mot de taille 2*n
+     */   
     public static long catalan2(int n) {
         long tabC[];
         tabC = new long [n+1];
@@ -56,6 +100,13 @@ public class MotBienParenthese {
         return tabC[n];
     }
     
+    /**
+     * Methode fournit par l'ennoncé.
+     * Mesure le temps de calcul de la methode catalan(int) au rang de 10 à n.
+     * 
+     * @param n
+     *          teste de 10 jusqu'à n
+     */   
     public static void testCatalan(int n){
         long catalani;
         long t0;
@@ -68,6 +119,16 @@ public class MotBienParenthese {
         }
     }
     
+    /**
+     * Mesure le temps de calcul de la methode catalan2(int) au rang [2500, 5000, 10000 , ... , n].
+     * Ceci pour montrer la complexité quadratique de catalan2(int): N*2 -> T*4.
+     * MAIS ATTENTION: pour mettre en valeur la complexité quadratique on doit aller
+     * à des valeurs de n pour lesquelles le resultat de catalan n'est plus exact
+     * à cause d la taille des 'long' (dernière valeur valide pour catalan(35).
+     * 
+     * @param n
+     *          teste de 2500 jusqu'à n en multipliant par 2 à chaque fois.
+     */   
     public static void testCatalan2(int n){
         long catalani;
         long t0;
@@ -80,6 +141,14 @@ public class MotBienParenthese {
         }
     }
     
+    /**
+     * vérifie qu'un mot de paranthése est bien paranthésé. Utillise l'altitude.
+     * 
+     * Complexité linéaire, O(n)
+     * 
+     * @param MP
+     *          mot de paranthése.
+     */
     public static boolean verifMBP(String MP) {
         int size = MP.length();
         int altitude = 0;
@@ -100,15 +169,39 @@ public class MotBienParenthese {
         }
     }
     
-    public static void ligneMBP(String MBP) {
+    /**
+     * Indente 'comme il faut' les paranthéses d'un mot bien paranthésé:
+     * la paranthése fermante ferme la sa paranthése ouvrante sur la même colonne.
+     * 
+     * Complexité linéaire, O(n)
+     * 
+     * @param MBP
+     *          mot bien paranthésé.
+     */
+    public static void indentMBP(String MBP) {
         int size = MBP.length();
         String space = "";
         for (int i = 0; i < size; i++) {
-            System.out.println(space + MBP.charAt(i));
-            space += " ";
+            if (MBP.charAt(i) == '(') {
+                System.out.println(space + MBP.charAt(i));
+                space += "    ";
+            }else{
+                String tmp = space.substring(0, (space.length() - 4));
+                space = tmp;
+                System.out.println(space + MBP.charAt(i));
+            }
         }
     }
     
+    /**
+     * Renvoi l'altitude d'un mot BP.
+     * ex: (()(())) -> altitude = 3
+     * 
+     * Complexité linéaire, O(n)
+     * 
+     * @param MP
+     *          mot de paranthése.
+     */
     public static int altitudeMBP(String MP) {
         int size = MP.length();
         int altitude = 0;
--- src/TestMotBienParenthese.java
+++ src/TestMotBienParenthese.java
@@ -7,18 +7,18 @@ public class TestMotBienParenthese {
         for (int i = 1; i < 5; i++) {
             for (String s : MotBienParenthese.enumMotsBP(i)) {
                 System.out.println(s);
-                }
+            }
             System.out.println("+++++++++++++++++++++++");
         }
-        
+
         System.out.println("catalan(8): " + MotBienParenthese.catalan(8));
         System.out.println("catalan2(8): " + MotBienParenthese.catalan2(8));
         //C8=1430
-        System.out.println("+++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++ CATALAN");
         
         MotBienParenthese.testCatalan(20);
         System.out.println("EXPONENTIEL O(e(n)). N+1 -> T*3");
-        System.out.println("+++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++ CATALAN2");
         MotBienParenthese.testCatalan2(200000);
         System.out.println("QUADRATIQUE O(n²). N*2 -> T*4");
         System.out.println("+++++++++++++++++++++++");
@@ -27,7 +27,7 @@ public class TestMotBienParenthese {
         System.out.println("((()) :" + MotBienParenthese.verifMBP("((())"));
         System.out.println("+++++++++++++++++++++++");
         
-        MotBienParenthese.ligneMBP("(())()");
+        MotBienParenthese.indentMBP("(())()");
         System.out.println("+++++++++++++++++++++++");
         System.out.println("profondeur de (()(((())))())) : " + MotBienParenthese.altitudeMBP("(()(((())))()))"));
 
